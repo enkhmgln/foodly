8284cc6ed185e3250ca9c5fa3cf225d77c500c6f
enkhmgln <enkhamgalan560@gmail.com>
2026-02-16 00:06:45 +0800

    Add Firebase configuration files and dependencies for mobile application integration, including Firebase options and initialization in app configuration. Update project files for Android, iOS, and macOS to support Firebase services.

--- mobile/lib/app_config.dart
+++ mobile/lib/app_config.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 
 import '/core/shared/store_manager.dart';
+import '/firebase_options.dart';
 
 abstract class AppConfig {
   AppConfig._();
 
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     await UserStoreManager.shared.init();
     await DeviceStoreManager.shared.init();
   }
